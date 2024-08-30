cc6dede474b3fa8298669828e457e17b778c072f
kierandrewett <kieran@dothq.org>
2024-08-30 15:17:39 +0100

    Remove import to pkgs

--- flake.nix
+++ flake.nix
@@ -56,6 +56,6 @@
                 };
             };
 
-            packages = helpers.forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+            packages = helpers.forAllSystems (system: import nixpkgs.legacyPackages.${system});
         };
 }
