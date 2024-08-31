2b0de2cce926dcb40cb333aafb5c65a40bae5dad
kierandrewett <kieran@dothq.org>
2024-08-31 22:07:39 +0100

    Enforce root for installer script

--- scripts/install_fresh.sh
+++ scripts/install_fresh.sh
@@ -2,15 +2,22 @@
 
 SYS_INSTALL_ERROR_HEADER="\033[1;31mERROR! Failed to install system.\n\e[0m"
 
+SYS_INSTALL_GIT_REPO=https://github.com/kierandrewett/system.git
 SYS_INSTALL_LUKS_KEY=/tmp/luks.key
 
 set -e
 
-export SOPS_AGE_KEY_FILE=secrets/key.txt
+if [ "$EUID" -ne 0 ]
+    printf "$SYS_INSTALL_ERROR_HEADER"
+    echo "The installer script will need to be ran as root."
+    exit 1
+fi
+
+export SOPS_AGE_KEY_FILE=$(pwd)/secrets/key.txt
 
 if [ ! -f $SOPS_AGE_KEY_FILE ]; then
     printf "$SYS_INSTALL_ERROR_HEADER"
-    echo "SOPS Age Key does not exist at secrets/key.txt."
+    echo "SOPS Age Key does not exist at $SOPS_AGE_KEY_FILE."
     echo "Supply the key and try the installer again."
     exit 1
 fi
@@ -26,3 +33,8 @@ sops decrypt --extract '["luks"]["passphrase"]' secrets/secrets.yaml > $SYS_INST
 # Stopgap to check if the key file exists
 ls $SYS_INSTALL_LUKS_KEY
 
+# Enter a shell with git available
+nix-shell -p git
+
+mkdir -p /flake
+git clone $SYS_INSTALL_GIT_REPO /flake
