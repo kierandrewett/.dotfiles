50b1bb098904902c8b37052eb381482c2c8c3368
kierandrewett <kieran@dothq.org>
2024-09-03 20:14:31 +0100

    Add disk configuration for ssociety

---
+++ sys/machines/ssociety/disks.nix
@@ -0,0 +1,46 @@
+_:
+{
+    disko.devices = {
+        disk = {
+            # WD PC SN740 1TB M.2 NVMe
+            nvme0 = {
+                type = "disk";
+                device = "/dev/disk/by-id/nvme-WD_PC_SN740_SDDPMQD-1T00-1101-232961802977";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        boot = {
+                            size = "1024M";
+                            type = "EF00";
+                            content = {
+                                type = "filesystem";
+                                format = "vfat";
+                                mountpoint = "/boot";
+                                mountOptions = [
+                                    "defaults"
+                                ];
+                            };
+                        };
+                        luks = {
+                            size = "100%";
+                            content = {
+                                type = "luks";
+                                name = "crypted";
+                                passwordFile = "/tmp/luks.key";
+                                settings = {
+                                    allowDiscards = true;
+                                };
+                                content = {
+                                    type = "btrfs";
+                                    extraArgs = [ "-f" ];
+                                    mountpoint = "/";
+                                    mountOptions = [ "compress=zstd" "noatime" ];
+                                };
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+}
