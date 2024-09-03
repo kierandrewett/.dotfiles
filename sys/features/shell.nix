011e88236d11128b9219fe6b6f643254a3db397c
kierandrewett <kieran@dothq.org>
2024-09-03 20:55:36 +0100

    Add fastfetch as system package

--- sys/features/shell.nix
+++ sys/features/shell.nix
@@ -1,5 +1,12 @@
-_:
+{
+    pkgs,
+    ...
+}:
 {
     # Super rudimentary zsh config for everyone else
     programs.zsh.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        pkgs.fastfetch
+    ];
 }
