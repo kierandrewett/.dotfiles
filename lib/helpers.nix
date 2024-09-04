d8c1f918da23016fd910c3469ca4a7bc046722a0
kierandrewett <kieran@dothq.org>
2024-09-04 21:24:26 +0100

    Import home a different way

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -26,7 +26,7 @@
                 {
                     home-manager.useGlobalPkgs = true;
                     home-manager.useUserPackages = true;
-                    home-manager.users.${username} = import ../home;
+                    home-manager.users.${username}.imports = [../home];
                 }
 
                 ../sys
