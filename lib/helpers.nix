c5eca211e530d49412a1752c2f8b3b0a87fcc0e5
kierandrewett <kieran@dothq.org>
2024-09-04 21:27:31 +0100

    Add inputs as extra special args for home manager

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -27,6 +27,7 @@
                     home-manager.useGlobalPkgs = true;
                     home-manager.useUserPackages = true;
                     home-manager.users.${username}.imports = [../home];
+                    home-manager.extraSpecialArgs.inputs = inputs;
                 }
 
                 ../sys
