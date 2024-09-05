51e6f86b78f7f8b6966c263b0d90061be55e9b40
kierandrewett <kieran@dothq.org>
2024-09-06 00:07:50 +0100

    Fetch headline zsh theme

--- home/features/shell.nix
+++ home/features/shell.nix
@@ -3,6 +3,11 @@
     config,
     ...
 }:
+let
+    zsh-headline = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/moarram/headline/master/headline.zsh-theme";
+    };
+in
 {
     home = {
         packages = with pkgs; [
@@ -20,16 +25,22 @@
         enable = true;
 
         initExtra = "fastfetch";
+        promptInit = ''
+            source ${zsh-headline}
+        '';
 
         history = {
             size = 10000;
             path = "${config.xdg.dataHome}/zsh/history";
         };
 
+
         oh-my-zsh = {
             enable = true;
-            plugins = ["git"];
-            theme = "headline";
+            plugins = [
+                "git"
+            ];
+            theme = "custom";
         };
     };
 }
