4d3f1d4ffe51cae2fa97eee0697e99bf88f9a239
kierandrewett <kieran@dothq.org>
2024-09-06 00:49:23 +0100

    Install non-standard browsers

--- home/apps/browsers/default.nix
+++ home/apps/browsers/default.nix
@@ -1,16 +1,20 @@
 {
+    pkgs,
     inputs,
     platform,
     ...
 }:
 {
+    environment.systemPackages = with pkgs; [
+        microsoft-edge
+        vivaldi
+        tor-browser
+    ];
+
     programs.firefox = {
         enable = true;
         package = inputs.firefox-nightly.packages.${platform}.firefox-nightly-bin;
     };
 
     programs.google-chrome.enable = true;
-    programs.microsoft-edge.enable = true;
-    programs.vivaldi.enable = true;
-    programs.tor-browser.enable = true;
 }
