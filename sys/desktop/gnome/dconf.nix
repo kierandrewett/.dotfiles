d465ec2f3cbe636971b89c4ea82fd5f15f85a472
kierandrewett <kieran@dothq.org>
2024-09-05 20:50:17 +0100

    Set picture to zoom for GNOME background

--- sys/desktop/gnome/dconf.nix
+++ sys/desktop/gnome/dconf.nix
@@ -10,11 +10,13 @@
 
             settings = {
                 "org/gnome/desktop/background" = {
+                    picture-options = "zoom";
                     picture-uri = "${pkgs.gnome-backgrounds}/share/backgrounds/gnome/adwaita-l.jxl";
                     picture-uri-dark = "${pkgs.gnome-backgrounds}/share/backgrounds/gnome/adwaita-d.jxl";
                 };
 
                 "org/gnome/desktop/screensaver" = {
+                    picture-options = "zoom";
                     picture-uri = "${pkgs.gnome-backgrounds}/share/backgrounds/gnome/adwaita-l.jxl";
                 };
             };
