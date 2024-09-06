ad7ed8ed621f7a818b37b55bf0d46ebe47fa08e3
kierandrewett <kieran@dothq.org>
2024-09-06 19:03:50 +0100

    Remove trailing slash for easyfx dconf

--- home/features/audio.nix
+++ home/features/audio.nix
@@ -9,7 +9,7 @@
         preset = "Default";
     };
 
-    dconf.settings."com/github/wwmm/easyeffects/" = {
+    dconf.settings."com/github/wwmm/easyeffects" = {
         last-loaded-output-preset = config.services.easyeffects.preset;
     };
 }
