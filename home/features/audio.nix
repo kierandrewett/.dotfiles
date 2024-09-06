8286938d87eef7db8576289134dac3c832e33719
kierandrewett <kieran@dothq.org>
2024-09-06 19:01:59 +0100

    Load easyfx preset using dconf

--- home/features/audio.nix
+++ home/features/audio.nix
@@ -1,10 +1,15 @@
 {
     pkgs,
+    config,
     ...
 }:
 {
     services.easyeffects = {
         enable = true;
         preset = "Default";
+    };
+
+    dconf.settings."com/github/wwmm/easyeffects/" = {
+        last-loaded-output-preset = config.services.easyeffects.preset;
     };
 }
