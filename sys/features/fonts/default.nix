48f11d116ea2e94ded9d28144dfb53a962b2bd43
kierandrewett <kieran@dothq.org>
2024-09-06 15:13:53 +0100

    Move segoe ui font inline

--- sys/features/fonts/default.nix
+++ sys/features/fonts/default.nix
@@ -3,8 +3,23 @@
     ...
 }:
 let
-    segoe-ui = import ./segoe-ui.nix;
-in 
+    segoe-ui = pkgs.stdenv.mkDerivation rec {
+        pname = "ttf-segoe-ui";
+        version = "2024-09-06";
+
+        src = pkgs.fetchFromGitHub {
+            owner = "mrbvrz";
+            repo = "segoe-ui-linux";
+            rev = "a89213b7136da6dd5c3638db1f2c6e814c40fa84";
+            hash = "1gsscyahcfia5hnsi9sakd435ffimhccjwv4y2q8cqqm28bvzjhg";
+        };
+
+        installPhase = ''
+            mkdir -p $out/share/fonts/truetype
+            install -m644 $src/font/*.ttf $out/share/fonts/truetype/
+        '';
+    };
+in
 {
     environment.systemPackages = with pkgs; [
         geist-font
