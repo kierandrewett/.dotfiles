0ab915481b83e1f424b57db760f6085658bbd67d
kierandrewett <kieran@dothq.org>
2024-09-06 03:17:35 +0100

    Use custom desktop file for vesktop

--- home/apps/chat/default.nix
+++ home/apps/chat/default.nix
@@ -4,6 +4,19 @@
 }:
 {
     home.packages = with pkgs; [
-        vesktop
+        (vesktop.overrideAttrs (oldAttrs: {
+            desktopItems = [
+                (makeDesktopItem {
+                    name = "vesktop";
+                    desktopName = "Discord";
+                    exec = "vesktop %U";
+                    icon = "discord";
+                    startupWMClass = "Vesktop";
+                    genericName = "Internet Messenger";
+                    keywords = [ "discord" "vencord" "electron" "chat" ];
+                    categories = [ "Network" "InstantMessaging" "Chat" ];
+                })
+            ];
+        }))
     ];
 }
