a89709257aae086c5c1182cd6724e7887a464ae8
kierandrewett <kieran@dothq.org>
2024-09-06 14:28:11 +0100

    Use latest spotify version

--- home/apps/media/spotify.nix
+++ home/apps/media/spotify.nix
@@ -1,14 +1,7 @@
-_:
-let
-    pkgs = import (builtins.fetchGit {
-        name = "nixpkgs-spotify-1.1.84.716.gc5f8b819";
-        url = "https://github.com/NixOS/nixpkgs/";
-        ref = "refs/heads/nixpkgs-unstable";
-        rev = "1b7a6a6e57661d7d4e0775658930059b77ce94a4";
-    }) {};
-
-    spotifyPkg = pkgs.spotify;
-in
 {
-    home.packages = [spotifyPkg];
+    pkgs,
+    ...
+}:
+{
+    home.packages = with pkgs; [spotify];
 }
