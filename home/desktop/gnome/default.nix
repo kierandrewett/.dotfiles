d40996d67822c2e98221dc8b7aae78df8338aca1
kierandrewett <kieran@dothq.org>
2024-09-06 03:40:04 +0100

    Add extensions

--- home/desktop/gnome/default.nix
+++ home/desktop/gnome/default.nix
@@ -1,7 +1,15 @@
-_:
+{
+    pkgs,
+    ...
+}:
 {
     imports = [
         ./dconf.nix
         ./extensions.nix
     ];
+
+    programs.kdeconnect = {
+        enable = true;
+        package = pkgs.gnomeExtensions.gsconnect;
+    };
 }
--- home/desktop/gnome/extensions.nix
+++ home/desktop/gnome/extensions.nix
@@ -5,7 +5,16 @@
 }:
 let
     extensions = with pkgs.gnomeExtensions; [
+        appindicator
+        bluetooth-battery
+        color-picker
         dash-to-dock
+        do-not-disturb-while-screen-sharing-or-recording
+        impatience
+        mpris-label
+        night-theme-switcher
+        resource-monitor
+        steal-my-focus-window
     ];
 in
 with pkgs.gnomeExtensions; {
@@ -14,6 +23,11 @@ with pkgs.gnomeExtensions; {
             disable-user-extensions = false;
             disabled-extensions = [];
             enabled-extensions = builtins.map (extension: extension.extensionUuid) extensions;
+        };
+
+        "org/gnome/shell/extensions/color-picker" = lib.mkIf (lib.elem color-picker extensions) {
+            enable-shortcut = true;
+            color-picker-shortcut = "['<Super>comma']";
         };
 
         "org/gnome/shell/extensions/dash-to-dock" = lib.mkIf (lib.elem dash-to-dock extensions) {
@@ -26,6 +40,24 @@ with pkgs.gnomeExtensions; {
             require-pressure-to-show = false;
             running-indicator-style = "DOTS";
             dash-max-icon-size = 56;
+        };
+
+        "org/gnome/shell/extensions/mpris-label" = lib.mkIf (lib.elem mpris-label extensions) {
+            auto-switch-to-most-recent = true;
+            extension-place = "right";
+            left-padding = 4;
+            right-padding = 4;
+            first-field = "xesam:title";
+            second-field = "";
+            last-field = "xesam:artist";
+            divider-string = " - ";
+            max-string-length = 20;
+            refresh-rate = 1000;
+            left-click-action = "activate-player";
+        };
+
+        "org/gnome/shell/extensions/nighttimeswitcher" = lib.mkIf (lib.elem night-theme-switcher extensions) {
+            manual-schedule = false;
         };
     };
 }
