d262ece6545af4916a994d6a9c1bc3f7199ab9a4
kierandrewett <kieran@dothq.org>
2024-09-06 21:21:01 +0100

    Add custom name to NextDNS

--- sys/features/networking/default.nix
+++ sys/features/networking/default.nix
@@ -4,10 +4,15 @@
     lib,
     ...
 }:
+let
+    safeHostname = builtins.replaceStrings [" "] ["--"] hostname;
+
+    nextdnsHostname = "${safeHostname}-729ec3.dns.nextdns.io";
+in
 {
     imports = [
         ./firewall.nix
-        
+
         ./wifi/vm.nix
     ];
 
@@ -17,13 +22,33 @@
         useDHCP = lib.mkForce true;
 
         nameservers = [
-            "1.1.1.1"
-            "1.0.0.1"
+            "45.90.28.0#${nextdnsHostname}"
+            "45.90.30.0#${nextdnsHostname}"
+            "1.1.1.1#one.one.one.one"
+            "1.0.0.1#one.one.one.one"
         ];
 
         networkmanager = {
             enable = true;
+            dns = "systemd-resolved";
         };
+    };
+
+    services.resolved = {
+        enable = true;
+
+        dnssec = "true";
+        dnsovertls = "true";
+        domains = [ "~." ];
+
+        fallbackDns = [
+            "45.90.28.0#${nextdnsHostname}"
+            "2a07:a8c0::#${nextdnsHostname}"
+            "45.90.30.0#${nextdnsHostname}"
+            "2a07:a8c1::#${nextdnsHostname}"
+            "1.1.1.1#one.one.one.one"
+            "1.0.0.1#one.one.one.one"
+        ];
     };
 
     users.users.${username}.extraGroups = [
