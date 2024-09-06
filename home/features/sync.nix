d0125c4f3edba7ff5274917f7e82fadc07649bb5
kierandrewett <kieran@dothq.org>
2024-09-07 00:19:45 +0100

    Remove extra curly brackets

--- home/features/sync.nix
+++ home/features/sync.nix
@@ -14,9 +14,9 @@
         Service = {
             ExecStart = "${pkgs.writeShellScript "rclone-sync" ''
                 #!/run/current-system/sw/bin/bash
-                RSYNC_URL=$(cat ${config.sops.secrets."sync/nc/url".path}})
-                RSYNC_USER=$(cat ${config.sops.secrets."sync/nc/username".path}})
-                RSYNC_PASS=$(cat ${config.sops.secrets."sync/nc/password".path}})
+                RSYNC_URL=$(cat ${config.sops.secrets."sync/nc/url".path})
+                RSYNC_USER=$(cat ${config.sops.secrets."sync/nc/username".path})
+                RSYNC_PASS=$(cat ${config.sops.secrets."sync/nc/password".path})
 
                 rsync \
                     --webdav-url $RSYNC_URL \
