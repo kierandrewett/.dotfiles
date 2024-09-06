56a539a3195f86e154d5a034128864003d12c082
kierandrewett <kieran@dothq.org>
2024-09-06 02:53:05 +0100

    Disable telemetry

--- home/apps/browsers/firefox.nix
+++ home/apps/browsers/firefox.nix
@@ -95,6 +95,32 @@ in
                     "extensions.autoDisableScopes" = 0; # Automatically enable extensions
                     "extensions.update.enabled" = false; # Disable extension updates as this is handled by Nix
 
+                    # Disable telemetry
+                    "app.shield.optoutstudies.enabled" = false;
+                    "browser.discovery.enabled" = false;
+                    "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+                    "browser.newtabpage.activity-stream.telemetry" = false;
+                    "browser.ping-centre.telemetry" = false;
+                    "datareporting.healthreport.service.enabled" = false;
+                    "datareporting.healthreport.uploadEnabled" = false;
+                    "datareporting.policy.dataSubmissionEnabled" = false;
+                    "datareporting.sessions.current.clean" = true;
+                    "devtools.onboarding.telemetry.logged" = false;
+                    "toolkit.telemetry.archive.enabled" = false;
+                    "toolkit.telemetry.bhrPing.enabled" = false;
+                    "toolkit.telemetry.enabled" = false;
+                    "toolkit.telemetry.firstShutdownPing.enabled" = false;
+                    "toolkit.telemetry.hybridContent.enabled" = false;
+                    "toolkit.telemetry.newProfilePing.enabled" = false;
+                    "toolkit.telemetry.prompted" = 2;
+                    "toolkit.telemetry.rejected" = true;
+                    "toolkit.telemetry.reportingpolicy.firstRun" = false;
+                    "toolkit.telemetry.server" = "";
+                    "toolkit.telemetry.shutdownPingSender.enabled" = false;
+                    "toolkit.telemetry.unified" = false;
+                    "toolkit.telemetry.unifiedIsOptIn" = false;
+                    "toolkit.telemetry.updatePing.enabled" = false;
+
                     "general.autoScroll" = true;
 
                     "widget.use-xdg-desktop-portal.file-picker" = 1;
@@ -108,29 +134,6 @@ in
                     user_pref("browser.uiCustomization.state", ${builtins.toJSON customizable-ui});
                 '';
             };
-        };
-
-
-        # https://mozilla.github.io/policy-templates/
-        policies = {
-            DisableFirefoxStudies = true;
-            DisableTelemetry = true;
-
-            DontCheckDefaultBrowser = true;
-
-            FirefoxHome = {
-                SponsoredTopSites = false;
-                SponsoredPocket = false;
-            };
-
-            FirefoxSuggest = {
-                SponsoredSuggestions = false;
-                ImproveSuggest = false;
-            };
-
-            # Not needed, we have Bitwarden
-            OfferToSaveLogins = false;
-            PasswordManagerEnabled = false;
         };
     };
 }
