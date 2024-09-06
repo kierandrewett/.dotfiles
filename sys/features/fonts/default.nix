2272308ab6d50f78088998cab0ad3922cdeac818
kierandrewett <kieran@dothq.org>
2024-09-06 15:14:55 +0100

    Use sha256 hash for segoe ui

--- sys/features/fonts/default.nix
+++ sys/features/fonts/default.nix
@@ -11,7 +11,7 @@ let
             owner = "mrbvrz";
             repo = "segoe-ui-linux";
             rev = "a89213b7136da6dd5c3638db1f2c6e814c40fa84";
-            hash = "1gsscyahcfia5hnsi9sakd435ffimhccjwv4y2q8cqqm28bvzjhg";
+            sha256 = "1gsscyahcfia5hnsi9sakd435ffimhccjwv4y2q8cqqm28bvzjhg";
         };
 
         installPhase = ''
