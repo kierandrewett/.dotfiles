6a162bf0efdb3cfcd9493e35dc8ca4620b352bea
kierandrewett <kieran@dothq.org>
2024-09-07 01:14:50 +0100

    Adjust caching params

--- home/features/sync.nix
+++ home/features/sync.nix
@@ -33,19 +33,28 @@ in
 
                 rclone --config /tmp/rclone-nc.conf \
                     mount nc: ${mountDir} \
-                    --dir-cache-time 48h \
+                    --fast-list \
+                    --dir-cache-time 168h \
                     --vfs-cache-mode full \
-                    --vfs-cache-max-age 48h \
-                    --vfs-read-chunk-size 10M \
-                    --vfs-read-chunk-size-limit 512M \
+                    --vfs-cache-max-age 168h \
+                    --vfs-read-chunk-size 64M \
+                    --vfs-read-chunk-size-limit 2048M \
+                    --max-read-ahead 256M \
+                    --poll-interval 1m \
                     --no-modtime \
-                    --buffer-size 512M
+                    --buffer-size 512M \
+                    --timeout 10m \
+                    --transfers 16 \
+                    --checkers 12 \
+                    --fuse-flag=sync_read \
+                    --fuse-flag=auto_cache \
+                    -v
             ''}";
             ExecStop = "${pkgs.writeShellScript "rclone-umount" ''
                 fusermount -u ${mountDir}
             ''}";
             Type = "notify";
-            Restart = "always";
+            Restart = "on-failure";
             RestartSec = "2s";
         };
     };
