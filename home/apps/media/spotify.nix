1dc0481f2d2d06381a050a9be96e9ce2d3feb30d
kierandrewett <kieran@dothq.org>
2024-09-11 17:54:12 +0100

    Inherit platform

--- home/apps/media/spotify.nix
+++ home/apps/media/spotify.nix
@@ -9,6 +9,8 @@ let
         url = "https://github.com/NixOS/nixpkgs/archive/7d0ed7f2e5aea07ab22ccb338d27fbe347ed2f11.tar.gz";
         sha256 = "0v27szwci00zi1gpznqs1lhalvhfhh19gxd8yb8brxi2mb1kc0bb";
     }) {
+        system = platform;
+        
         config = { allowUnfree = true; };
     };
 in
