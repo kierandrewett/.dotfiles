4a22f3e721fd5fe5061f00f194fa9601a949bd6a
kierandrewett <kieran@dothq.org>
2024-09-11 17:53:16 +0100

    Add hash to spotify pkgs

--- home/apps/media/spotify.nix
+++ home/apps/media/spotify.nix
@@ -5,8 +5,11 @@
     ...
 }:
 let
-    pkgSpotify = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7d0ed7f2e5aea07ab22ccb338d27fbe347ed2f11.tar.gz") {
-        config =  { allowUnfree = true; };
+    pkgSpotify = import (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/7d0ed7f2e5aea07ab22ccb338d27fbe347ed2f11.tar.gz";
+        sha256 = "0v27szwci00zi1gpznqs1lhalvhfhh19gxd8yb8brxi2mb1kc0bb";
+    }) {
+        config = { allowUnfree = true; };
     };
 in
 {
