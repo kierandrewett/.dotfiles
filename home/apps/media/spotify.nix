814577d0e836bc188c137f2f5840328dbd082fa7
kierandrewett <kieran@dothq.org>
2024-09-11 17:05:15 +0100

    Use older version of Spotify

--- home/apps/media/spotify.nix
+++ home/apps/media/spotify.nix
@@ -2,6 +2,13 @@
     pkgs,
     ...
 }:
+let
+    nixpkgsWithOldSpotify = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/1b7a6a6e57661d7d4e0775658930059b77ce94a4.tar.gz";
+    }) {};
+
+    spotify = nixpkgsWithOldSpotify.spotify;
+in
 {
-    home.packages = with pkgs; [spotify];
+    home.packages = [spotify];
 }
